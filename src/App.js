bc700f9ea916410fdf02ab407f27aba16d54a34d
Thein Zaw Myo <theinzawmyo40@gmail.com>
2021-11-10 19:30:17 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Service from "./services/Service";
 import Weather from "./components/Weather";
